f157b08865f9d1913fc8e499569305965ace6e4d
jathanism <jathan.mccollum@teamaol.com>
2012-07-11 09:16:14 -0700

    Fixed ImportError causing setup.py to crash

--- trigger/__init__.py
+++ trigger/__init__.py
@@ -3,5 +3,3 @@ __version__ = (1, 2, 0)
 full_version = '.'.join(str(x) for x in __version__)
 release = full_version
 short_version = '.'.join(str(x) for x in __version__[0:3])
-
-from . import exceptions
