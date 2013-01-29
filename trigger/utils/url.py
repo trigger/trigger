40f0386e5e530df27ace64bced2cd973a36bf860
jathanism <jathanism@aol.com>
2013-01-29 09:21:41 -0800

    - Fix to make parse_url support file paths or URLs

--- trigger/utils/url.py
+++ trigger/utils/url.py
@@ -17,47 +17,47 @@ except ImportError:
 
 def _parse_url(url):
     """
-    Stub for `~trigger.utils.url.parse_url`
+    Guts for `~trigger.utils.url.parse_url`.
 
-    Borrowed from Kombu's ``kombu.utils.url``.
+    Based on Kombu's ``kombu.utils.url``.
     Source: http://bit.ly/11UFcfH
     """
-    scheme = urlparse(url).scheme
-    schemeless = url[len(scheme) + 3:]
-    # parse with HTTP URL semantics
-    parts = urlparse('http://' + schemeless)
-
+    parts = urlparse(url)
+    scheme = parts.scheme
     port = parts.port or None
     hostname = parts.hostname
     path = parts.path or ''
-    path = path[1:] if path and path[0] == '/' and scheme != 'file' else path
     virtual_host = path[1:] if path and path[0] == '/' else path
     return (scheme, unquote(hostname or '') or None, port,
             unquote(parts.username or '') or None,
             unquote(parts.password or '') or None,
             unquote(path or '') or None,
             unquote(virtual_host or '') or None,
+            unquote(parts.query or '') or None,
             dict(dict(parse_qsl(parts.query))))
 
 def parse_url(url):
     """
     Given a ``url`` returns, a dict of its constituent parts.
 
-    Borrowed from Kombu's ``kombu.utils.url``.
+    Based on Kombu's ``kombu.utils.url``.
     Source: http://bit.ly/11UFcfH
 
     :param url:
         Any standard URL. (file://, https://, etc.)
     """
-    scheme, host, port, user, password, path, virtual_host, query = _parse_url(url)
-    return dict(transport=scheme, hostname=host,
-                port=port, userid=user,
-                password=password, path=path, virtual_host=virtual_host, **query)
+    scheme, host, port, user, passwd, path, vhost, qs, qs_dict = _parse_url(url)
+    return dict(scheme=scheme, hostname=host, port=port, username=user,
+                password=passwd, path=path, virtual_host=vhost,
+                query=qs, **qs_dict)
 
 if __name__ == '__main__':
     tests = (
         "https://username:password@myhost.aol.com:12345?limit=10&vendor=cisco#develop",
         "file:///usr/local/etc/netdevices.xml",
+        '/usr/local/etc/netdevices.xml',
+        'mysql://dbuser:dbpass@dbhost.com:3306/',
+        'http://jathan:password@api.foo.com/netdevices/?limit=10&device_type=switch&vendor=cisco&format=json',
     )
 
     import pprint
