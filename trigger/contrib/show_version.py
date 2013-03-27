bc6190e2e1f40a5efd0d2c594fda32828817ec6c
Mike Biancaniello <m.biancaniello@teamaol.com>
2013-03-26 22:55:29 -0400

    Added show_version plugin.

---
+++ trigger/contrib/show_version.py
@@ -0,0 +1,27 @@
+from trigger.contrib.commando import CommandoApplication
+
+import re
+import os.path
+from socket import getfqdn, gethostbyname
+import xml.etree.ElementTree as ET
+#from twisted.python import failure
+from xml.etree.cElementTree import ElementTree, Element, SubElement
+from trigger.utils import xmltodict, strip_juniper_namespace
+
+from twisted.python import log
+
+task_name = 'show_version'
+class_name = 'ShowVersion'
+
+class ShowVersion(CommandoApplication):
+    """Simple example to run ``show version`` on devices."""
+    commands = ['show version']
+
+def xmlrpc_show_version(self, creds, devices):
+    """Run 'show version' on the specified list of `devices`"""
+    log.msg('Creating ShowVersion')
+    sc = ShowVersion(devices=devices, creds=creds)
+    log.msg('Done creating ShowVersion')
+    d = sc.run()
+    return d
+
