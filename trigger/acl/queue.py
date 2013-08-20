33c70ea7007a1a7cd2e4835b3ad52bbbb997e33a
jathanism <jathanism@aol.com>
2013-08-20 13:13:28 -0700

    First pass at using SQLite vs. MySQL for acl.queue
    - Bundling 'peewee' as internal dep to do ORM.

--- trigger/acl/queue.py
+++ trigger/acl/queue.py
@@ -1,8 +1,8 @@
-#coding=utf-8
+# -*- coding: utf-8 -*-
 
 """
 Database interface for automated ACL queue. Used primarily by ``load_acl`` and
-``acl``` commands for manipulating the work queue. 
+``acl``` commands for manipulating the work queue.
 
     >>> from trigger.acl.queue import Queue
     >>> q = Queue()
@@ -14,16 +14,22 @@ Database interface for automated ACL queue. Used primarily by ``load_acl`` and
 __author__ = 'Jathan McCollum'
 __maintainer__ = 'Jathan McCollum'
 __email__ = 'jathan.mccollum@teamaol.com'
-__copyright__ = 'Copyright 2010-2011, AOL Inc.'
+__copyright__ = 'Copyright 2010-2012, AOL Inc.'
 
 import os
 import sys
+from trigger import exceptions
 from trigger.conf import settings
 from trigger.netdevices import NetDevices
 try:
     import MySQLdb
 except ImportError:
     MySQLdb = None
+
+try:
+    import sqlite3
+except ImportError:
+    sqlite3 = None
 
 # Exports
 __all__ = ('Queue', 'QueueError',)
@@ -32,7 +38,7 @@ __all__ = ('Queue', 'QueueError',)
 class QueueError(Exception): pass
 
 # Classes
-class Queue(object):
+class BaseQueue(object):
     """
     Interacts with firewalls database to insert/remove items into the queue. You
     may optionally suppress informational messages by passing ``verbose=False``
@@ -48,19 +54,11 @@ class Queue(object):
         self.verbose = verbose
 
     def _get_firewall_db_conn(self):
-        """Returns a MySQL db connection used for the ACL queues using database
-        settings found withing ``settings.py``."""
-        if MySQLdb is None:
-            raise RuntimeError("You must install ``MySQL-python`` to use the queue")
-        try:
-            return MySQLdb.connect(host=settings.DATABASE_HOST,
-                                   db=settings.DATABASE_NAME,
-                                   port=settings.DATABASE_PORT,
-                                   user=settings.DATABASE_USER,
-                                   passwd=settings.DATABASE_PASSWORD)
-        # catch if we can't connect, and shut down
-        except MySQLdb.OperationalError as e:
-            sys.exit("Can't connect to the database - %s (error %d)" % (e[1],e[0]))
+        """
+        Returns a db connection for the ACL queues using database settings found
+        within ``settings.py``.
+        """
+        raise NotImplementedError("You define this method in your subclass!")
 
     def _normalize(self, arg):
         if arg.startswith('acl.'):
@@ -73,13 +71,13 @@ class Queue(object):
 
     def insert(self, acl, routers, escalation=False):
         """
-        Insert an ACL and associated devices into the ACL load queue.  
+        Insert an ACL and associated devices into the ACL load queue.
 
-        Attempts to insert into integrated queue.  If ACL test fails, then 
+        Attempts to insert into integrated queue.  If ACL test fails, then
         item is inserted into manual queue.
         """
         assert acl, 'no ACL defined'
-        if not routers: 
+        if not routers:
             routers = []
 
         (escalation, acl) = self._normalize(acl)
@@ -88,15 +86,15 @@ class Queue(object):
                 try:
                     dev = self.nd.find(router)
                 except KeyError:
-                    raise  "Could not find %s in netdevices" % router
+                    raise exceptions.TriggerError("Could not find %s in netdevices" % router)
                     return
                 if acl not in dev.acls:
-                    raise "Could not find %s in %s's acl list" % (acl, router)
+                    raise exceptions.TriggerError("Could not find %s in %s's acl list" % (acl, router))
                     return
 
-                self.cursor.execute('''insert into acl_queue 
-                                        (acl, router, queued, escalation) 
-                                        values (%s, %s, now(), %s)''', 
+                self.cursor.execute('''insert into acl_queue
+                                        (acl, router, queued, escalation)
+                                        values (?, ?, datetime('now'), ?)''',
                                         (acl, router, escalation))
 
             if self.verbose:
@@ -114,10 +112,10 @@ class Queue(object):
         """
         Delete an ACL from the firewall database queue.
 
-        Attempts to delete from integrated queue.  If ACL test fails, then 
+        Attempts to delete from integrated queue.  If ACL test fails, then
         item is deleted from manual queue.
         """
-        assert acl, 'no ACL defined' 
+        assert acl, 'no ACL defined'
         (escalation, acl) = self._normalize(acl)
 
         if routers is not None:
@@ -125,21 +123,21 @@ class Queue(object):
         else:
             if self.verbose: print 'fetching routers from database'
             self.cursor.execute('''select distinct router from acl_queue
-                                where acl = %s and loaded is null
+                                where acl = ? and loaded is null
                                 order by router''', (acl,))
             rows = self.cursor.fetchall()
             devs = [row[0] for row in rows] or []
 
         if len(devs):
             for dev in devs:
-                self.cursor.execute('''delete from acl_queue where acl=%s and 
-                                    router=%s and loaded is null''', (acl, dev))
+                self.cursor.execute('''delete from acl_queue where acl=? and
+                                    router=? and loaded is null''', (acl, dev))
 
             if self.verbose:
                 print 'ACL', acl, 'cleared from integrated load queue for',
                 print ', '.join([dev[:dev.find('.')] for dev in devs])
         elif self.cursor.execute('''delete from queue
-                                where q_name = %s and done = 0''', (acl,)):
+                                where q_name = ? and done = 0''', (acl,)):
             if self.verbose: print '"%s" cleared from manual load queue' % acl
         else:
             if self.verbose: print '"%s" not found in manual or integrated queues' % acl
@@ -154,12 +152,14 @@ class Queue(object):
         current timestampe.  Migrated from clear_load_queue() in load_acl.
         """
         for acl in acls:
-            self.cursor.execute("""update acl_queue set loaded=now() where
-                                acl=%s and router=%s and loaded is null""",
+            self.cursor.execute("""update acl_queue set loaded=datetime('now') where
+                                acl=? and router=? and loaded is null""",
                                 (acl, device))
-        if self.verbose: 
+        if self.verbose:
             print 'Marked the following ACLs as complete for %s: ' % device
             print ', '.join(acls)
+
+        self.dbconn.commit()
 
     def remove(self, acl, routers, escalation=False):
         """
@@ -170,13 +170,15 @@ class Queue(object):
         troubleshooting or addressing errors with automated loads.  This leaves
         the items in the database but removes them from the active queue.
         """
-        assert acl, 'no ACL defined' 
+        assert acl, 'no ACL defined'
         for router in routers:
-            self.cursor.execute("""update acl_queue set loaded=0 where acl=%s
-                                and router=%s and loaded is null""", (acl, router))
-        if self.verbose: 
+            self.cursor.execute("""update acl_queue set loaded=0 where acl=?
+                                and router=? and loaded is null""", (acl, router))
+        if self.verbose:
             print 'Marked the following devices as removed for ACL %s: ' % acl
             print ', '.join(routers)
+
+        self.dbconn.commit()
 
     def list(self, queue='integrated', escalation=False):
         """
@@ -187,7 +189,7 @@ class Queue(object):
         if queue == 'integrated':
             query = 'select distinct router, acl from acl_queue where loaded is null'
             if escalation:
-                query += ' and escalation = true' 
+                query += ' and escalation = true'
         elif queue == 'manual':
             query = 'select q_name, login, q_ts, done from queue where done=0'
         else:
@@ -200,3 +202,33 @@ class Queue(object):
         self.dbconn.commit()
 
         return all_data
+
+class SQLiteQueue(BaseQueue):
+    def _get_firewall_db_conn(self):
+        """Returns a SQLite db connection used for the ACL queues using database
+        settings found withing ``settings.py``."""
+        if sqlite3 is None:
+            raise RuntimeError("You must have ``sqlite3`` to use the queue")
+        try:
+            return sqlite3.connect(database=settings.DATABASE_HOST)
+        # catch if we can't connect, and shut down
+        except sqlite3.Error as e:
+            sys.exit("Can't connect to the database - %s (error %d)" % (e[1],e[0]))
+Queue = SQLiteQueue
+
+class MySQLQueue(BaseQueue):
+    def _get_firewall_db_conn(self):
+        """Returns a MySQL db connection used for the ACL queues using database
+        settings found withing ``settings.py``."""
+        if MySQLdb is None:
+            raise RuntimeError("You must install ``MySQL-python`` to use the queue")
+        try:
+            return MySQLdb.connect(host=settings.DATABASE_HOST,
+                                   db=settings.DATABASE_NAME,
+                                   port=settings.DATABASE_PORT,
+                                   user=settings.DATABASE_USER,
+                                   passwd=settings.DATABASE_PASSWORD)
+        # catch if we can't connect, and shut down
+        except MySQLdb.OperationalError as e:
+            sys.exit("Can't connect to the database - %s (error %d)" % (e[1],e[0]))
+
