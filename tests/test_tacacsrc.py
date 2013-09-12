1a299037d18902cf3d1a69d46fe70b5962de72ba
jathanism <jathan@gmail.com>
2013-09-11 17:33:15 -0700

    Fix Tacacsrc tests (see below):
    + fixes #117 - test_tacacsrc.TacacsrcTest.testWrite fails
    + fixes #118 - test_tacacsrc.TacacsrcTest.testRead fails
    
    Change-Id: Iffd5b35778b837ce97ec10bb907d13615b9f3422

--- tests/test_tacacsrc.py
+++ tests/test_tacacsrc.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Michael Shields <shieldszero@aol.com>'
+__author__ = 'Jathan McCollum, Michael Shields'
 __maintainer__ = 'Jathan McCollum'
-__copyright__ = 'Copyright 2005-2011 AOL Inc.'
-__version__ = '1.5'
+__copyright__ = 'Copyright 2005-2011 AOL Inc.; 2013 Salesforce.com'
+__version__ = '2.0'
 
 
 from StringIO import StringIO
 import os
 import unittest
+import tempfile
+from trigger.tacacsrc import Tacacsrc, Credentials
 
-from trigger.tacacsrc import Tacacsrc
 
-RIGHT_CREDS = ('jschmoe', 'abc123')
+# Constants
+RIGHT_CREDS = Credentials('jschmoe', 'abc123', 'aol')
 RIGHT_TACACSRC = {
-    'version': '2.0',
     'aol_uname_': 'jschmoe',
     'aol_pwd_': 'abc123',
 }
+TACACSRC = os.getenv('TACACSRC')
 
 
-def miniparser(s):
-    lines = [line.strip() for line in s.split('\n')]
-    lines = [line for line in lines if not line.startswith('#')]
-    return dict([line.split(' = ') for line in lines if len(line)])
-
+def miniparser(data, tcrc):
+    """Manually parse .tacacsrc lines into a dict"""
+    lines = [line.strip() for line in data]
+    lines = [line for line in lines if line and not line.startswith('#')]
+    ret = {}
+    for line in lines:
+        k, _, v = line.partition(' = ')
+        ret[k] = tcrc._decrypt_old(v)
+    return ret
 
 class Testing_Tacacsrc(Tacacsrc):
     def _get_keyfile_nonce(self):
@@ -41,15 +47,24 @@ class TacacsrcTest(unittest.TestCase):
 
     def testWrite(self):
         """Test writing .tacacsrc."""
-        buf = StringIO()
-        t = Testing_Tacacsrc(False)
+        _, file_name = tempfile.mkstemp('_tacacsrc')
+        t = Testing_Tacacsrc(generate_new=False)
         t.creds['aol'] = RIGHT_CREDS
-        t.file = buf
+        # Overload the default file_name w/ our temp file
+        t.file_name = file_name
         t.write()
-        output = miniparser(buf.getvalue())
-        self.assertEqual(output, RIGHT_TACACSRC)
-        self.assertEqual(output, miniparser(file(os.getenv('TACACSRC')).read()))
 
+        # Read the file we wrote back in and check it against what we think it
+        # should look like.
+        output = miniparser(t._read_file_old(), t)
+        self.assertEqual(output, RIGHT_TACACSRC)
+
+        # And then compare it against the manually parsed value using
+        # miniparser()
+        with open(TACACSRC, 'r') as fd:
+            lines = fd.readlines()
+            self.assertEqual(output, miniparser(lines, t))
+        os.remove(file_name)
 
 if __name__ == "__main__":
     unittest.main()
