685d877487e5f47cf5b3b529e24ad62444474998
shoujin.wang <shoujin.wang@salesforce.com>
2014-03-26 13:29:55 -0700

    Added unit_test.sh and acceptance_test.sh

---
+++ acceptance_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+########################################
+#
+# Author : Autumn Wang
+# Date   : 03/26/2014
+#
+# This is the wrapper shell script to 
+# run acceptance test.
+#
+#########################################
+
+BASEDIR=$(dirname $0)
+
+export PATH=/opt/bin:${PATH}
+export TRIGGER_TEST_DIR=${BASEDIR}/tests/acceptance
+export TRIGGER_SETTINGS=${TRIGGER_TEST_DIR}/data/settings.py
+export NETDEVICES_SOURCE=${TRIGGER_TEST_DIR}/data/netdevices.xml
+export BOUNCE_FILE=${TRIGGER_TEST_DIR}/data/bounce.py
+
+python2.7 -W ignore::RuntimeWarning ${BASEDIR}/tests/acceptance/trigger_acceptance_tests.py
