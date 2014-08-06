880b7d8d41f83ad58d2523eedaa6a3d0059fa10b
Joseph Malone <joseph@josephmalone.com>
2014-08-06 13:50:41 -0600

    Got basic SRX grammar working :)

--- trigger/juniper_srx.py
+++ trigger/juniper_srx.py
@@ -20,8 +20,9 @@ __version__ = '1.2.2'
 
 import IPy
 from trigger.acl.parser import (Protocol, check_range, literals, TIP,
-                                do_protocol_lookup, make_nondefault_processor,
-                                ACLParser, ACLProcessor, default_processor, S)
+        do_protocol_lookup, make_nondefault_processor,
+        ACLParser, ACLProcessor, default_processor, S, errs, juniper_multiline_comments,
+        settings, fragment_flag_names, ip_option_names, tcp_flag_names, rules)
 from trigger.acl.tools import create_trigger_term
 from trigger import exceptions
 
@@ -29,13 +30,19 @@ from trigger import exceptions
 # TODO (jathan): Implement __all__
 __all__ = ()
 
+def braced_list(arg):
+    '''Returned braced output.  Will alert if comment is malformed.'''
+    #return '("{", jws?, (%s, jws?)*, "}")' % arg
+    return '("{", jws?, (%s, jws?)*, "}"!%s)' % (arg, errs['comm_start'])
+
+
 # Classes
 class JuniperSRX(object):
     """
     Parses and generates Juniper SRX firewall policy.
     """
     def __init__(self):
-        self.applications   = SRXApplications()
+        #self.applications   = SRXApplications()
         self.interfaces     = []
         self.address_groups = []
         self.service_groups = []
@@ -62,7 +69,8 @@ class JuniperSRX(object):
             'tcp':       ('"tcp" / "6"', Protocol('tcp')),
             'udp':       ('"udp" / "17"', Protocol('udp')),
             'icmp':      ('"icmp" / "1"', Protocol('icmp')),
-            'root':      'ws?, netscreen, ws?',
+            #root is used as "production" for the parser. This is the beginning of the parsing tree
+            S('root'):     'jws?, "security", jws?, "{", ws, policies, ws, "}"',
             #
             # Junos general grammar stuff (copied from /trigger/acl/junos.py)
             #
@@ -85,6 +93,31 @@ class JuniperSRX(object):
             #
             # Juniper SRX-specific grammar (some inspiration from /trigger/acl/junos.py)
             # TODO: come up with this grammar
+            
+            #jws? DEMANDS A space!!!
+#             S('policies'):                  '"policies", jws?, "{", jws?, from_to_zone_section+, jws?, "}"',
+
+            S('policies'):                  '"policies", jws?,' + braced_list('from_to_zone_section+'),
+            S('from_to_zone_section'):      '"from-zone", jws?, from_zone, jws?, "to-zone", jws?, to_zone, jws?,' + braced_list('policy+'),
+            'from_zone':                    'jword',
+            'to_zone':                      'jword',
+            S('policy'):                    'jws?, "policy", jws?, alphanums, jws?,' + braced_list('match / then'),
+            S('match'):                     '"match", jws?,' + braced_list('source_address / destination_address / application'),
+            S('source_address'):            '"source-address", jws?, ipv4/address_obj, jws?, ";"',
+            S('destination_address'):       '"destination-address", jws?, ipv4/address_obj, jws?, ";"',
+            S('application'):               '"application", jws?, jword, jws?, ";"',
+            'address_obj':                  'jword',
+            S('then'):                      '"then", jws?, "{", jws?, action, jws?, ";", jws?, log*, jws?, "}"',
+            'action':                       'jword',
+            'log':                          '"log", jws?, "{", jws?, "session-init;", jws?, "session-close;", jws?, "}"',
+
+
+         
+
+#acl = parser.parse("filter 123 { term T1 { from { destination-address { 10.20.30.40/32; } protocol tcp; destination-port 80; } then { accept; } } }")
+
+
+
         }
 
         for production, rule, in rules.iteritems():
@@ -111,13 +144,10 @@ class JuniperSRX(object):
             return multi
         return single
 
-    def braced_list(arg):
-        '''Returned braced output.  Will alert if comment is malformed.'''
-        #return '("{", jws?, (%s, jws?)*, "}")' % arg
-        return '("{", jws?, (%s, jws?)*, "}"!%s)' % (arg, errs['comm_start'])
 
     def parse(self, data):
         """Parse policy into list of NSPolicy objects."""
+        #print self.grammar
         parser = ACLParser(self.grammar)
         try:
             string = data.read()
@@ -125,6 +155,13 @@ class JuniperSRX(object):
             string = data
 
         success, children, nextchar = parser.parse(string)
+
+        #print success
+        #print children
+        #print nextchar
+        #print string
+        #print data
+        #print len(string)
 
         if success and nextchar == len(string):
             assert len(children) == 1
