320ca933fe0835e5ff578c5d856544981a53fd03
coxley <codey@cwv.net>
2015-01-25 03:49:42 -0500

    Version joining inconsistent with map and list comprehension.

--- trigger/__init__.py
+++ trigger/__init__.py
@@ -1,5 +1,6 @@
 __version__ = (1, 4, 9)
 
-full_version = '.'.join(map(str, __version__[0:3])) + ''.join(__version__[3:])
+full_version = '.'.join(str(x) for x in __version__[0:3]) + \
+               ''.join(__version__[3:])
 release = full_version
 short_version = '.'.join(str(x) for x in __version__[0:3])
