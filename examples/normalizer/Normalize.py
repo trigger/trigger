ebe87f45a841bc4b70098a7753029b5894c4ce26
John Marrett <johnf@zioncluster.ca>
2015-08-20 14:12:01 -0400

    Flake8 Test Changes

--- examples/normalizer/Normalize.py
+++ examples/normalizer/Normalize.py
@@ -13,143 +13,160 @@ from twisted.internet import defer, task, reactor
 from Router import Router
 import jsonpickle
 
+
 class getRouterDetails(ReactorlessCommando):
-	commands = Router.showCommands
+    commands = Router.showCommands
 
-	def select_next_device(self, jobs=None):
-		"""
-		Select another *reachable* device.
-		"""
-		if jobs is None:
-			jobs = self.jobs
+    def select_next_device(self, jobs=None):
+        """
+        Select another *reachable* device.
+        """
+        if jobs is None:
+            jobs = self.jobs
 
-		next_device = jobs.pop()
-		log.msg('Selecting next device: %s' % next_device)
+        next_device = jobs.pop()
+        log.msg('Selecting next device: %s' % next_device)
 
-		# If I ping, return me.
-		if next_device.is_reachable():
-			log.msg('PING [SUCCESS]: %s' % next_device)
-			return next_device
-		# Otherwise return None and store me as an error.
-		else:
-			msg = 'PING [FAILURE]: %s' % next_device
-			log.msg(msg)
-			print "Failed to ping host {}".format(next_device)
-			if self.verbose:
-				print msg
-			return None
+        # If I ping, return me.
+        if next_device.is_reachable():
+            log.msg('PING [SUCCESS]: %s' % next_device)
+            return next_device
+        # Otherwise return None and store me as an error.
+        else:
+            msg = 'PING [FAILURE]: %s' % next_device
+            log.msg(msg)
+            print "Failed to ping host {}".format(next_device)
+            if self.verbose:
+                print msg
+            return None
 
-	def errback(self, failure, device):
-		print "Error in getRouterDetails for device {}\n{}".format(device,failure.getTraceback())
+    def errback(self, failure, device):
+        print "Error in getRouterDetails for device {}\n{}".format(
+            device,
+            failure.getTraceback()
+        )
+
 
 def validateRouterDetails(result):
-	print "Validating router details"
-	devicesToCorrect = []
+    print "Validating router details"
+    devicesToCorrect = []
 
-	for device, results in result.items():
-		routers[device].results = results
-		routers[device].validate()
-		routers[device].normalize()
-		if routers[device].normalizeRequired:
-			devicesToCorrect.append(device)
+    for device, results in result.items():
+        routers[device].results = results
+        routers[device].validate()
+        routers[device].normalize()
+        if routers[device].normalizeRequired:
+            devicesToCorrect.append(device)
 
-	return devicesToCorrect or None
+    return devicesToCorrect or None
+
 
 class normalizeRouters(ReactorlessCommando):
-	def to_cisco(self, dev, commands=None, extra=None):
-		dev_commands = routers[dev.nodeName].commands
-		# self.commands = dev_commands
-		# print "Device {}: Executing Commands:\n{}".format(dev.nodeName,dev_commands)
-		return dev_commands
+    def to_cisco(self, dev, commands=None, extra=None):
+        dev_commands = routers[dev.nodeName].commands
+        # self.commands = dev_commands
+        # print "Device {}: Executing Commands:\n{}".format(
+        #       dev.nodeName, dev_commands)
+        return dev_commands
 
-	def from_cisco(self, results, device, commands=None):
-		commands = commands or self.commands
+    def from_cisco(self, results, device, commands=None):
+        commands = commands or self.commands
 
-		# dev_commands = routers[device.nodeName].commands
-		# print "In from_cisco for {}, storing results {} from commands {}".format(device,results,self.commands)
-		log.msg('Received %r from %s' % (results, device))
-		self.store_results(device, self.map_results(commands, results))
+        log.msg('Received %r from %s' % (results, device))
+        self.store_results(device, self.map_results(commands, results))
 
-	def errback(self, failure, device):
-		print "Error in normalizeRouters for device {}\n{}".format(device,failure.getTraceback())
+    def errback(self, failure, device):
+        print "Error in normalizeRouters for device {}\n{}".format(
+            device,
+            failure.getTraceback()
+        )
 
 
 def initiateRouterNormalization(devices):
-	#log.startLogging(sys.stdout, setStdout=False)
-	if devices is not None:
-		print "Normalizing {} devices ({})".format(len(devices)," ".join(devices))
-		devicesToNormalize = []
-		for device in devices:
-			if routers[device].normalizeRequired:
-				devicesToNormalize.append(device)
-		return normalizeRouters(devicesToNormalize).run()
-	else:
-		print "No devices need to be normalized".format()
-	return None
+    # log.startLogging(sys.stdout, setStdout=False)
+    if devices is not None:
+        print "Normalizing {} devices ({})".format(
+            len(devices),
+            " ".join(devices)
+        )
+        devicesToNormalize = []
+        for device in devices:
+            if routers[device].normalizeRequired:
+                devicesToNormalize.append(device)
+        return normalizeRouters(devicesToNormalize).run()
+    else:
+        print "No devices need to be normalized".format()
+    return None
+
 
 def stop_reactor(result):
-	if reactor.running:
-		reactor.stop()
-		return result 
+    if reactor.running:
+        reactor.stop()
+        return result
 
 if __name__ == '__main__':
 
-	nd = NetDevices()
-	device_list=[]
-	routers={}
+    nd = NetDevices()
+    device_list = []
+    routers = {}
 
-	try:
-		stateFile = open("routerstate.json","r")
-		data = stateFile.read()
-		stateFile.close()
-		routers = jsonpickle.decode(data)
-		data = None
-	except Exception as e:
-		print("Failed to open routerstate.json")
+    try:
+        stateFile = open("routerstate.json", "r")
+        data = stateFile.read()
+        stateFile.close()
+        routers = jsonpickle.decode(data)
+        data = None
+    except Exception as e:
+        print("Failed to open routerstate.json")
 
-	# Accept a list of routers and argument or parse test-units.csv
-	if len(sys.argv) > 1:
-		device_list = sys.argv[1:]
-	else:
-		print "Processing all sites"
-		with open('test-units.csv', 'rb') as csvfile:
-			spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-			for row in spamreader:
-				device_list.append(row[0])
-		user_response = raw_input("Are you certain you want to normalize {} devices? [y/N]\n".format(len(device_list)))
-		m = re.search ("^[yY]",user_response)
-		if m is None:
-			print "Aborting processing"
-			exit(1)
+    # Accept a list of routers and argument or parse test-units.csv
+    if len(sys.argv) > 1:
+        device_list = sys.argv[1:]
+    else:
+        print "Processing all sites"
+        with open('test-units.csv', 'rb') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in spamreader:
+                device_list.append(row[0])
+        user_response = raw_input(
+            "Are you certain you want to normalize {} devices? [y/N]\n".format(
+                len(device_list)
+            )
+        )
+        m = re.search("^[yY]", user_response)
+        if m is None:
+            print "Aborting processing"
+            exit(1)
 
-	device_list = map(lambda x:x.lower(),device_list)
+    device_list = map(lambda x: x.lower(), device_list)
 
-	print "Processing {} devices ({})".format(len(device_list)," ".join(device_list))
+    print "Processing {} devices ({})".format(
+        len(device_list),
+        " ".join(device_list)
+    )
 
-	for device in device_list:
-		if not device in routers:
-			routers[device]=Router(device)
+    for device in device_list:
+        if device not in routers:
+            routers[device] = Router(device)
 
-	# log.startLogging(sys.stdout, setStdout=False)
+    # log.startLogging(sys.stdout, setStdout=False)
 
-	d = getRouterDetails(device_list).run()
-	d.addCallback(validateRouterDetails)
-	d.addCallback(initiateRouterNormalization)
-	d.addBoth(stop_reactor)
+    d = getRouterDetails(device_list).run()
+    d.addCallback(validateRouterDetails)
+    d.addCallback(initiateRouterNormalization)
+    d.addBoth(stop_reactor)
 
-	reactor.run()
+    reactor.run()
 
-	if d.result is not None:
-		for device in d.result.keys():
-			# print "Device {}: Command Output:\n{}".format(device,d.result[device][None])
-			m = re.search(r"\[OK\]",d.result[device]["write mem"])
-			if m is not None:
-				print "Device {}: Configuration Saved".format(device)
-			else:
-				print "Device {}: Warning no [OK] in Output".format(device)
+    if d.result is not None:
+        for device in d.result.keys():
+            m = re.search(r"\[OK\]", d.result[device]["write mem"])
+            if m is not None:
+                print "Device {}: Configuration Saved".format(device)
+            else:
+                print "Device {}: Warning no [OK] in Output".format(device)
 
-	stateFile = open("routerstate.json","w")
-	jsonpickle.set_encoder_options('json', sort_keys=True, indent=4)
-	data = jsonpickle.encode(routers)
-	stateFile.write(data)
-
+    stateFile = open("routerstate.json", "w")
+    jsonpickle.set_encoder_options('json', sort_keys=True, indent=4)
+    data = jsonpickle.encode(routers)
+    stateFile.write(data)
--- examples/normalizer/Router.py
+++ examples/normalizer/Router.py
@@ -3,48 +3,56 @@ from datetime import datetime, timedelta
 
 
 class Router(object):
-	showCommands = ['show run | i ip access-list','show ver']
+    showCommands = ['show run | i ip access-list', 'show ver']
 
-	def __init__(self, name):
-		self.name = name
-		self.acls = []
-		self.commands=[]
-		self.normalizeRequired = False
-		self.version = None
-		self.results = {}
-		self.commando = None
-		self.lastAccess = None
+    def __init__(self, name):
+        self.name = name
+        self.acls = []
+        self.commands = []
+        self.normalizeRequired = False
+        self.version = None
+        self.results = {}
+        self.commando = None
+        self.lastAccess = None
 
-	def validate(self):
-		self.lastAccess=datetime.now()
-		self.validateSystem()
-		self.validateAcl()
+    def validate(self):
+        self.lastAccess = datetime.now()
+        self.validateSystem()
+        self.validateAcl()
 
-	def validateSystem(self):
-		for line in self.results["show ver"].splitlines():
-			line=line.strip()
-			m = re.search("^Cisco IOS Software.*Version (\S+), ",line)
-			if m is not None:
-				self.version=m.group(1)
+    def validateSystem(self):
+        for line in self.results["show ver"].splitlines():
+            line = line.strip()
+            m = re.search("^Cisco IOS Software.*Version (\S+), ", line)
+            if m is not None:
+                self.version = m.group(1)
 
-	def validateAcl(self):
-		self.acls=[]
-		for line in self.results["show run | i ip access-list"].splitlines():
-			line=line.strip()
-			m = re.search("ip access-list \S+ (\S+)",line)
-			if m is not None:
-				self.acls.append(m.group(1))
+    def validateAcl(self):
+        self.acls = []
+        for line in self.results["show run | i ip access-list"].splitlines():
+            line = line.strip()
+            m = re.search("ip access-list \S+ (\S+)", line)
+            if m is not None:
+                self.acls.append(m.group(1))
 
-	def normalize(self):
-		self.commands=[]
-		self.normalizeRequired=False
-		if not "trigger-test-1" in self.acls:
-			self.normalizeRequired=True
-			self.commands+=["ip access-list standard trigger-test-1","permit 1.1.1.1"]
-			print "{}: Will normalized trigger-test acl on device".format(self.name)
+    def normalize(self):
+        self.commands = []
+        self.normalizeRequired = False
+        if "trigger-test-1" not in self.acls:
+            self.normalizeRequired = True
+            self.commands += [
+                "ip access-list standard trigger-test-1",
+                "permit 1.1.1.1"
+            ]
+            print "{}: Will normalized trigger-test acl on device".format(
+                self.name
+            )
 
-		pre_commands=["conf t"]
-		post_commands=["end","write mem"]
-		self.commands=pre_commands+self.commands+post_commands
-		# self.commands=map(lambda x:x.encode("ascii"),self.commands)
-			# print "Commands to run on device {} are {}".format(device,routers[device].commands)
+        pre_commands = ["conf t"]
+        post_commands = ["end", "write mem"]
+        self.commands = pre_commands+self.commands+post_commands
+
+        # print "Commands to run on device {} are {}".format(
+        #     device,
+        #     routers[device].commands
+        # )
