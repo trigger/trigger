fb2befcbba33684c4c4eaafeb9728a456d03dadc
jathanism <jathan@gmail.com>
2026-02-03 08:43:28 -0800

    fix(configs): modernize Python 3 syntax and fix all ruff errors
    - Convert Python 2 print statements to Python 3 functions
    - Remove unnecessary UTF-8 encoding declarations (UP009)
    - Modernize percent formatting to f-strings (UP031)
    - Remove unused imports and fix import sorting
    - Fix ambiguous unicode quotes in comments (RUF003)
    - Add noqa comment for example password in trigger_settings.py
    - Remove BW alias, use full BounceWindow name (N817)
    - Fix operator precedence with parentheses (RUF021)
    - Apply ruff formatting for consistent style
    
    All configs/ files now pass ruff check and are Python 3 compatible.
    Do not exclude configs/ from ruff checks - these are user-facing examples.

--- configs/autoacl.py
+++ configs/autoacl.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
 
 """
 autoacl.py - This file controls when ACLs get auto-applied to network devices,
@@ -15,30 +14,29 @@ before editing and compare it to the output after your change.
 Skip down a bit to find the part you should edit.
 """
 
-import os
 import sys
 
 from twisted.python import log
-from trigger.conf import settings
-from trigger.acl import acl_exists
 
+from trigger.conf import settings
 
 # Exports
-__all__ = ('autoacl',)
+__all__ = ("autoacl",)
 
-#===============================
+# ===============================
 # BEGIN USER-SERVICEABLE PARTS
-#===============================
+# ===============================
 
-log.msg('IN CUSTOM AUTOACL.PY FILE:', __file__)
+log.msg("IN CUSTOM AUTOACL.PY FILE:", __file__)
 OWNERS = settings.VALID_OWNERS
+
 
 def autoacl(dev, explicit_acls=None):
     """
     Given a NetDevice object, returns a set of **implicit** (auto) ACLs. We require
     a device object so that we don't have circular dependencies between netdevices
     and autoacl.
-    
+
     This function MUST return a set() of acl names or you will break the ACL
     associations. An empty set is fine, but it must be a set!
 
@@ -54,89 +52,94 @@ def autoacl(dev, explicit_acls=None):
     # Explicitly pass a set of explicit_acls so that we can use it as a
     # dependency for assigning implicit_acls. Defaults to an empty set.
     if explicit_acls is None:
-        log.msg('[%s]: explicit_acls unset' % dev)
+        log.msg(f"[{dev}]: explicit_acls unset")
         explicit_acls = set()
 
     # Skip anything not owned by valid groups
     if dev.owningTeam not in OWNERS:
-        log.msg('[%s]: invalid owningTeam' % dev)
+        log.msg(f"[{dev}]: invalid owningTeam")
         return set()
 
     # Skip firewall devices
-    if dev.deviceType == 'FIREWALL':
-        log.msg('[%s]: firewall device' % dev)
+    if dev.deviceType == "FIREWALL":
+        log.msg(f"[{dev}]: firewall device")
         return set()
 
     # Prep acl set
-    log.msg('[%s]: autoacls initialized' % dev)
+    log.msg(f"[{dev}]: autoacls initialized")
     acls = set()
 
-    # 
+    #
     # ACL Magic starts here
-    # 
-    if dev.vendor in ('brocade', 'cisco', 'foundry'):
+    #
+    if dev.vendor in ("brocade", "cisco", "foundry"):
         log.msg("[%s]: adding ACLs ('118', '119')")
-        acls.add('118')
-        acls.add('119')
+        acls.add("118")
+        acls.add("119")
 
     #
     # Other GSR acls
     #
-    if dev.vendor == 'cisco':
-        log.msg("[%s]: adding ACLs ('117')" % dev)
-        acls.add('117')
-        if dev.make == '12000 SERIES' and dev.nodeName.startswith('pop') or dev.nodeName.startswith('bb'):
-            log.msg("[%s]: adding ACLs ('backbone-acl')" % dev)
-            acls.add('backbone-acl')
-        elif dev.make == '12000 SERIES':
-            log.msg("[%s]: adding ACLs ('gsr-acl')" % dev)
-            acls.add('gsr-acl')
+    if dev.vendor == "cisco":
+        log.msg(f"[{dev}]: adding ACLs ('117')")
+        acls.add("117")
+        if (
+            dev.make == "12000 SERIES" and dev.nodeName.startswith("pop")
+        ) or dev.nodeName.startswith("bb"):
+            log.msg(f"[{dev}]: adding ACLs ('backbone-acl')")
+            acls.add("backbone-acl")
+        elif dev.make == "12000 SERIES":
+            log.msg(f"[{dev}]: adding ACLs ('gsr-acl')")
+            acls.add("gsr-acl")
     #
     # Juniper acls
     #
-    if dev.vendor == 'juniper':
-        if dev.deviceType == 'SWITCH':
-            log.msg("[%s]: adding ACLs ('juniper-switch-protect')" % dev)
-            acls.add('juniper-switch-protect')
+    if dev.vendor == "juniper":
+        if dev.deviceType == "SWITCH":
+            log.msg(f"[{dev}]: adding ACLs ('juniper-switch-protect')")
+            acls.add("juniper-switch-protect")
         else:
-            log.msg("[%s]: adding ACLs ('juniper-router-protect')" % dev)
-            acls.add('juniper-router-protect')
-            acls.add('juniper-router.policer')
+            log.msg(f"[{dev}]: adding ACLs ('juniper-router-protect')")
+            acls.add("juniper-router-protect")
+            acls.add("juniper-router.policer")
 
     #
     # Explicit ACL example
     #
     # Only add acl '10' (or variants) to the device if 'acb123.special' is not
     # explicitly associated with the device.
-    if '10.special' in explicit_acls:
+    if "10.special" in explicit_acls:
         pass
-    elif dev.deviceType == 'ROUTER':
-        log.msg("[%s]: adding ACLs ('10')" % dev)
-        acls.add('10')
-    elif dev.deviceType == 'SWITCH':
-        log.msg("[%s]: adding ACLs ('10.sw')" % dev)
-        acls.add('10.sw')
+    elif dev.deviceType == "ROUTER":
+        log.msg(f"[{dev}]: adding ACLs ('10')")
+        acls.add("10")
+    elif dev.deviceType == "SWITCH":
+        log.msg(f"[{dev}]: adding ACLs ('10.sw')")
+        acls.add("10.sw")
 
     return acls
 
-#===============================
+
+# ===============================
 # END USER-SERVICEABLE PARTS
-#===============================
+# ===============================
+
 
 def main():
     """A simple syntax check and dump of all we see and know!"""
-    print 'Syntax ok.'
+    print("Syntax ok.")
     if len(sys.argv) > 1:
         from trigger.netdevices import NetDevices
+
         nd = NetDevices()
         names = sorted(nd)
         for name in names:
             dev = nd[name]
-            if dev.deviceType not in ('ROUTER', 'SWITCH'):
+            if dev.deviceType not in ("ROUTER", "SWITCH"):
                 continue
             acls = sorted(dev.acls)
-            print '%-39s %s' % (name, ' '.join(acls))
+            print(f"{name:<39} {' '.join(acls)}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
